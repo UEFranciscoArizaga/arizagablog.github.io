73c829fcb04bbbfdf7e857361ca9f20ade83a925
GuariVT <multiguari@gmail.com>
2026-01-16 23:41:42 -0500

    df

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES_REPO ? `/${process.env.GITHUB_PAGES_REPO}/` : '/',
+  base: '/arizagablog/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
